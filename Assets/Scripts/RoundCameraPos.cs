f61ee5ef03750dcd50313289f73d2a63d9820ea1
VladLinkevich <linkevichvv@mail.ru>
2020-11-30 13:11:37 +0300

    4 chapter is done

---
+++ Assets/Scripts/RoundCameraPos.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class RoundCameraPos : CinemachineExtension
+{
+    public float PixelPerUnit = 32;
+
+    protected override void PostPipelineStageCallback(CinemachineVirtualCameraBase vcam,
+        CinemachineCore.Stage stage,
+        ref CameraState state, float deltaTime)
+    {
+        if (stage == CinemachineCore.Stage.Body)
+        {
+            if (stage == CinemachineCore.Stage.Body)
+            {
+                Vector3 finalPos = state.FinalPosition;
+                Vector3 newPos = new Vector3(Round(finalPos.x), Round(finalPos.y), finalPos.z);
+
+                state.PositionCorrection += finalPos - newPos;
+            }
+        }
+    }
+
+    float Round(float x)
+    {
+        return Mathf.Round(x * PixelPerUnit) / PixelPerUnit;
+    }
+}
